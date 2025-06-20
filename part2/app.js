2f8d10f472e970fece947d5d11f2700d51717b38
LolfiOPAC <a1922997@student.adelaide.edu.au>
2025-06-20 18:54:53 +0000

    [automatic commit] 2025-06-20 18:54:52 UTC 180.150.112.77

--- part2/app.js
+++ part2/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const session = require('express-session'); // ✅ import this
+const session = require('express-session'); 
 require('dotenv').config();
 
 const app = express();
