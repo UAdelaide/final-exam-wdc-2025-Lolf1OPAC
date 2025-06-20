3949d7a3fac878b7a65bd3db956fc9fa56167de4
LolfiOPAC <a1922997@student.adelaide.edu.au>
2025-06-20 20:10:52 +0000

    [automatic commit] 2025-06-20 20:10:52 UTC 180.150.112.77

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -6,9 +6,9 @@ const db = require('../models/db');
 router.get('/mydogs', async (req, res) => {
   try {
     const ownerId = req.session.user.user_id;
-    // match the owner id with the matching dog ids in the data
+    // match the owner id with the matching dog ids in the database and then retrieve the name as well.
     const [dogs] = await db.query(`
-      SELECT dog_id, name, size FROM Dogs WHERE owner_id = ?
+      SELECT dog_id, name FROM Dogs WHERE owner_id = ?
     `, [ownerId]);
     res.json(dogs);
   } catch (error) {
