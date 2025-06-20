f88534fa7c0cd75ae75e2aaf93212d75b4131430
LolfiOPAC <a1922997@student.adelaide.edu.au>
2025-06-20 19:42:37 +0000

    [automatic commit] 2025-06-20 19:42:37 UTC 180.150.112.77

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -69,23 +69,6 @@ router.post('/logout', (req, res) => {
   }
 });
 
-// GET dogs owned by current logged-in owner
-router.get('/mydogs', async (req, res) => {
-  if (!req.session.user) {
-    return res.status(401).json({ error: 'Not logged in' });
-  }
-
-  try {
-    const ownerId = req.session.user.user_id;
-    const [dogs] = await db.query(`
-      SELECT dog_id, name, size FROM Dogs WHERE owner_id = ?
-    `, [ownerId]);
-
-    res.json(dogs);
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to fetch dogs' });
-  }
-});
 
 
 module.exports = router;
